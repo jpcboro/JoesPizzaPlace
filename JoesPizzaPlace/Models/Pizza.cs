62ef4940e54cb788a8d47e6dae10d026410801ec
joe boro <joepboro@gmail.com>
2024-05-21 10:07:31 +1000

    Add project files.

---
+++ JoesPizzaPlace/Models/Pizza.cs
@@ -0,0 +1,24 @@
+﻿namespace JoesPizzaPlace.Models
+{
+    public class Pizza
+    {
+        public int PizzaId { get; set; }
+        public string Name { get; set; } = string.Empty;
+   
+        public string? ShortDescription { get; set; }
+        public string? LongDescription { get; set; }
+        public string? AllergyInformation { get; set; }
+        public decimal Price { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? ImageThumbnailUrl { get; set; }
+        public bool IsVegetarian { get; set; }
+        public bool IsPizzaOfTheWeek { get; set; }
+        public bool InStock { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = default!;
+
+
+
+
+    }
+}
