2e088696f33e4d6a116fe1d1ead1ea47b948f855
joe boro <joepboro@gmail.com>
2024-05-21 10:22:47 +1000

    assign string.Empty to CategoryName - cannot be null

--- JoesPizzaPlace/Models/Category.cs
+++ JoesPizzaPlace/Models/Category.cs
@@ -3,7 +3,7 @@
     public class Category
     {
         public int CategoryId { get; set; }
-        public string CategoryName { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
         public string? Description { get; set; }
         public List<Pizza>? Pizzas { get; set; }
     }
